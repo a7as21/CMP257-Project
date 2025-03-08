f4d378942f7a248c7ff6c2a01e318d4d08305d2b
a7as21 <ahmed.alsuwaidi249@gmail.com>
2025-03-08 19:32:14 +0400

    navsidebar adjusted slightly

--- src/navsidebarimport.js
+++ src/navsidebarimport.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Load navbar dynamically
+
     fetch("src/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
         });
 
-    // Load sidebar dynamically
     fetch("src/sidebar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("sidebar-container").innerHTML = data;
 
-            // Wait for the sidebar to load before adding event listener
             document.getElementById("sidebarToggle").addEventListener("click", function () {
                 document.getElementById("sidebar").classList.toggle("collapsed");
             });
