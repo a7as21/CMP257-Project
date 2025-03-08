a088a940a24661199a441141a3c3083ecf23ab36
a7as21 <ahmed.alsuwaidi249@gmail.com>
2025-03-08 19:36:45 +0400

    removed garbage part of code

--- src/navsidebarimport.js
+++ src/navsidebarimport.js
@@ -10,9 +10,5 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("sidebar-container").innerHTML = data;
-
-            document.getElementById("sidebarToggle").addEventListener("click", function () {
-                document.getElementById("sidebar").classList.toggle("collapsed");
-            });
         });
 });
