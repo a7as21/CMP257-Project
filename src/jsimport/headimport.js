f78492bd718bf9afaea831c953fd7593b43ed05b
a7as21 <ahmed.alsuwaidi249@gmail.com>
2025-03-10 07:29:44 +0400

    test on profile.html

--- src/jsimport/headimport.js
+++ src/jsimport/headimport.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("src/reusable files/head.html")
+    fetch("../reusable files/head.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("head-container").outerHTML = data;
